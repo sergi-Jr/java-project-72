ed7f2a8f294229aacd0a2b548b8fb1d9e8a7c0ec
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-04-25 19:57:43 +0400

    Add util class provides Named routing

---
+++ app/src/main/java/hexlet/code/core/utils/NamedRoutes.java
@@ -0,0 +1,11 @@
+package hexlet.code.core.utils;
+
+public final class NamedRoutes {
+    public static String rootPath() {
+        return "/";
+    }
+
+    public static String urlsPath() {
+        return "/urls";
+    }
+}
