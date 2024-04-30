3940cc3ab1c6908742aecf9fcc7ff0706a234717
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-04-30 04:17:18 +0400

    [skip ci][skip render] Add url check handler

--- app/src/main/java/hexlet/code/core/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/core/controllers/UrlController.java
@@ -1,23 +1,31 @@
 package hexlet.code.core.controllers;
 
-import hexlet.code.abstracts.dto.BasePage;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import hexlet.code.core.dal.UrlCheckRepository;
 import hexlet.code.core.dal.UrlRepository;
 import hexlet.code.core.dto.BuildUrlPage;
 import hexlet.code.core.dto.UrlPage;
 import hexlet.code.core.dto.UrlsPage;
 import hexlet.code.core.models.Url;
+import hexlet.code.core.models.UrlCheck;
 import hexlet.code.core.utils.NamedRoutes;
+import hexlet.code.core.utils.ViewUtil;
 import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
 import io.javalin.http.NotFoundResponse;
+import org.javatuples.Pair;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.sql.Timestamp;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
 
@@ -29,64 +37,88 @@ public class UrlController {
 
     public static void create(Context context) {
         try {
-            String name = context.formParamAsClass("url", String.class)
-                    .get();
-
+            String name = context.formParamAsClass("url", String.class).get();
             URL url = URI.create(name).toURL();
             String urlString = buildUrlString(url);
-
-            Calendar calendar = Calendar.getInstance();
-            Timestamp timestamp = new Timestamp(calendar.getTimeInMillis());
-
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
             Url entity = new Url(urlString, timestamp);
+
             if (UrlRepository.save(entity)) {
-                setFlashesInContext(context, "Страница успешно добавлена", "success");
+                ViewUtil.setFlashesInContext(context, "Страница успешно добавлена", "success");
                 context.redirect(NamedRoutes.urlsPath(), HttpStatus.FOUND);
             } else {
-                setFlashesInContext(context, "Страница уже существует", "danger");
                 BuildUrlPage page = new BuildUrlPage(name);
-                setFlashesInPage(context, page);
+                ViewUtil.setFlashes(page, context, "Страница уже существует", "danger");
                 context.render("main.jte", model("page", page));
             }
         } catch (MalformedURLException | IllegalArgumentException exception) {
             String name = context.formParam("url");
             BuildUrlPage page = new BuildUrlPage(name);
-            setFlashesInContext(context, "Некорректный URL", "danger");
-            setFlashesInPage(context, page);
+            ViewUtil.setFlashes(page, context, "Некорректный URL", "danger");
             context.render("main.jte", model("page", page));
         }
-    }
-
-    private static void setFlashesInContext(Context ctx, String msg, String type) {
-        ctx.sessionAttribute("flash", msg);
-        ctx.sessionAttribute("flashType", type);
-    }
-
-    private static void setFlashesInPage(Context ctx, BasePage page) {
-        page.setFlash(ctx.consumeSessionAttribute("flash"));
-        page.setFlashType(ctx.consumeSessionAttribute("flashType"));
     }
 
     private static String buildUrlString(URL input) {
         String urlNoPort = String.format("%s://%s", input.getProtocol(), input.getHost());
         int port = input.getPort();
-        return port == -1 ?
-                urlNoPort :
-                String.format("%s:%d", urlNoPort, port);
+        return port == -1 ? urlNoPort : String.format("%s:%d", urlNoPort, port);
     }
 
     public static void index(Context context) {
+        List<Pair<Url, UrlCheck>> urlTuples = new ArrayList<>();
         List<Url> urls = UrlRepository.getEntities();
-        UrlsPage page = new UrlsPage(urls);
-        setFlashesInPage(context, page);
+        urls.forEach(u -> {
+            Optional<UrlCheck> check = UrlCheckRepository.findLast(u.getId());
+            check.ifPresentOrElse(c -> urlTuples.add(new Pair<>(u, c)),
+                    () -> urlTuples.add(new Pair<>(u, null)));
+        });
+        UrlsPage page = new UrlsPage(urlTuples);
+        ViewUtil.setFlashesInPage(context, page);
         context.render("urls/index.jte", model("page", page));
     }
 
     public static void show(Context context) {
         Long id = context.pathParamAsClass("id", Long.class).get();
         Url url = UrlRepository.find(id).orElseThrow(() ->
-                new NotFoundResponse("Странно... Не удалось найти такой url :("));
-        UrlPage page = new UrlPage(url.getName());
+                new NotFoundResponse("Could not find URL :("));
+        List<UrlCheck> checks = UrlCheckRepository.getChecks(id);
+        UrlPage page = new UrlPage(id, url.getName(), checks);
         context.render("urls/show.jte", model("page", page));
+    }
+
+    public static void check(Context context) {
+        Long id = context.pathParamAsClass("id", Long.class).get();
+        String url = context.formParamAsClass("url", String.class).get();
+        try {
+            HttpResponse<String> response = Unirest.get(url).asString();
+            String body = response.getBody();
+            UrlCheck check = new UrlCheck(
+                    response.getStatus(),
+                    getTextByRegex(regexTagsBetween("title"), body),
+                    getTextByRegex(regexTagsBetween("h1"), body),
+                    getTextByRegex("(?<=\"description\" content=\").+?(?=\">)", body),
+                    new Timestamp(System.currentTimeMillis())
+            );
+            check.setUrlId(id);
+            UrlCheckRepository.save(check);
+            ViewUtil.setFlashesInContext(context, "Проверка успешно добавлена", "success");
+            context.redirect(NamedRoutes.urlPath(id), HttpStatus.FOUND);
+        } catch (UnirestException e) {
+            List<UrlCheck> checks = UrlCheckRepository.getChecks(id);
+            UrlPage page = new UrlPage(id, url, checks);
+            ViewUtil.setFlashes(page, context, "Проверка не удалась", "danger");
+            context.render("urls/show.jte", model("page", page));
+        }
+    }
+
+    private static String regexTagsBetween(String tag) {
+        return String.format("(?<=<%s>).+?(?=</%s>)", tag, tag);
+    }
+
+    private static String getTextByRegex(String regex, String input) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find() ? matcher.group() : null;
     }
 }
