92f347d8ea035ff9d541f78729288b8a6c9a3a3f
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-04-30 04:22:04 +0400

    [skip ci][skip render] add check repo class

---
+++ app/src/main/java/hexlet/code/core/dal/UrlCheckRepository.java
@@ -0,0 +1,127 @@
+package hexlet.code.core.dal;
+
+import hexlet.code.abstracts.dal.BaseRepository;
+import hexlet.code.core.models.UrlCheck;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+public class UrlCheckRepository extends BaseRepository {
+    public static boolean save(UrlCheck entity) {
+        String query = "Insert into url_checks "
+                + "(title, url_id, status_code, h1, description, created_at) values (?, ?, ?, ?, ?, ?)";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement prep = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            prep.setString(1, entity.getTitle());
+            prep.setLong(2, entity.getUrlId());
+            prep.setInt(3, entity.getStatusCode());
+            prep.setString(4, entity.getH1());
+            prep.setString(5, entity.getDescription());
+            prep.setTimestamp(6, entity.getCreatedAt());
+            prep.executeUpdate();
+            ResultSet keys = prep.getGeneratedKeys();
+            if (keys.next()) {
+                entity.setId(keys.getLong(1));
+                return true;
+            } else {
+                log.error("",
+                        new SQLException("DB have not returned an id after save"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
+
+    public static Optional<UrlCheck> find(Long id) {
+        String query = "Select * from url_checks where id = ?";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement prep = conn.prepareStatement(query)) {
+            prep.setLong(1, id);
+            ResultSet resultSet = prep.executeQuery();
+            if (resultSet.next()) {
+                UrlCheck check = new UrlCheck(
+                        resultSet.getInt("status_code"),
+                        resultSet.getString("title"),
+                        resultSet.getString("h1"),
+                        resultSet.getString("description"),
+                        resultSet.getTimestamp("created_at")
+                );
+                check.setId(resultSet.getLong("id"));
+                check.setUrlId(id);
+                return Optional.of(check);
+            }
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Optional<UrlCheck> findLast(Long urlId) {
+        String query = "Select top 1 * from url_checks where url_id = ? order by created_at";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement prep = conn.prepareStatement(query)) {
+            prep.setLong(1, urlId);
+            ResultSet resultSet = prep.executeQuery();
+            if (resultSet.next()) {
+                UrlCheck check = new UrlCheck(
+                        resultSet.getInt("status_code"),
+                        resultSet.getString("title"),
+                        resultSet.getString("h1"),
+                        resultSet.getString("description"),
+                        resultSet.getTimestamp("created_at")
+                );
+                check.setId(resultSet.getLong("id"));
+                check.setUrlId(resultSet.getLong("url_id"));
+                return Optional.of(check);
+            }
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void delete(Long id) {
+        String query = "Delete from url_checks where id = ?";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement prep = conn.prepareStatement(query)) {
+            prep.setLong(1, id);
+            prep.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<UrlCheck> getChecks(Long id) {
+        String query = "Select * from url_checks where url_id = ? order by created_at desc";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement prep = conn.prepareStatement(query)) {
+            prep.setLong(1, id);
+            ResultSet set = prep.executeQuery();
+            LinkedList<UrlCheck> list = new LinkedList<>();
+            while (set.next()) {
+                UrlCheck check = new UrlCheck(
+                        set.getInt("status_code"),
+                        set.getString("title"),
+                        set.getString("h1"),
+                        set.getString("description"),
+                        set.getTimestamp("created_at")
+                );
+                check.setId(set.getLong("id"));
+                check.setUrlId(id);
+                list.add(check);
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
