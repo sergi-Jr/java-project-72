c6a02c2510267de547d6e74206d4a54793233fcc
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-04-30 04:19:10 +0400

    [skip ci][skip render] add url checks in jte templates

--- app/src/main/java/hexlet/code/core/dto/UrlPage.java
+++ app/src/main/java/hexlet/code/core/dto/UrlPage.java
@@ -1,11 +1,16 @@
 package hexlet.code.core.dto;
 
 import hexlet.code.abstracts.dto.BasePage;
+import hexlet.code.core.models.UrlCheck;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class UrlPage extends BasePage {
+    private Long id;
     private String name;
+    private List<UrlCheck> checks;
 }
--- app/src/main/java/hexlet/code/core/dto/UrlsPage.java
+++ app/src/main/java/hexlet/code/core/dto/UrlsPage.java
@@ -2,13 +2,15 @@ package hexlet.code.core.dto;
 
 import hexlet.code.abstracts.dto.BasePage;
 import hexlet.code.core.models.Url;
+import hexlet.code.core.models.UrlCheck;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.javatuples.Pair;
 
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class UrlsPage extends BasePage {
-    private List<Url> urls;
+    private List<Pair<Url, UrlCheck>> urlTuples;
 }
