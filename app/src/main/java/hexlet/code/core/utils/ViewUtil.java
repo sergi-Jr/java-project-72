86c6f25f37aa9788432250b312b75175b472e1a0
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-04-30 04:20:10 +0400

    [skip ci][skip render] add utility class for view manage

---
+++ app/src/main/java/hexlet/code/core/utils/ViewUtil.java
@@ -0,0 +1,21 @@
+package hexlet.code.core.utils;
+
+import hexlet.code.abstracts.dto.BasePage;
+import io.javalin.http.Context;
+
+public class ViewUtil {
+    public static void setFlashes(BasePage page, Context ctx, String msg, String type) {
+        setFlashesInContext(ctx, msg, type);
+        setFlashesInPage(ctx, page);
+    }
+
+    public static void setFlashesInContext(Context ctx, String msg, String type) {
+        ctx.sessionAttribute("flash", msg);
+        ctx.sessionAttribute("flashType", type);
+    }
+
+    public static void setFlashesInPage(Context ctx, BasePage page) {
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flashType"));
+    }
+}
