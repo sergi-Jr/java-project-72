4c1d270bef6af97226e19f9088ae0b7000da65f0
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-04-30 04:22:27 +0400

    [skip ci][skip render] add url check class

---
+++ app/src/main/java/hexlet/code/core/models/UrlCheck.java
@@ -0,0 +1,28 @@
+package hexlet.code.core.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+public class UrlCheck {
+    @Setter
+    private Long id;
+    private int statusCode;
+    private String title;
+    private String h1;
+    private String description;
+    @Setter
+    private Long urlId;
+    private Timestamp createdAt;
+
+
+    public UrlCheck(int statusCode, String title, String h1, String description, Timestamp createdAt) {
+        this.statusCode = statusCode;
+        this.title = title;
+        this.h1 = h1;
+        this.description = description;
+        this.createdAt = createdAt;
+    }
+}
