fef5bcd2a70346f0ab9297702e2520a43d7e9d62
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-04-30 04:23:13 +0400

    [skip ci][skip render] add url check table creating into the init.sql script

--- app/jte-classes/gg/jte/generated/ondemand/urls/JteindexGenerated.java
+++ app/jte-classes/gg/jte/generated/ondemand/urls/JteindexGenerated.java
@@ -3,18 +3,18 @@ import hexlet.code.core.dto.UrlsPage;
 import hexlet.code.core.utils.NamedRoutes;
 public final class JteindexGenerated {
 	public static final String JTE_NAME = "urls/index.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,3,3,3,5,5,8,8,19,19,21,21,21,22,22,22,22,22,22,22,22,22,22,22,22,26,26,29,29,29,29,29,3,3,3,3};
+	public static final int[] JTE_LINE_INFO = {0,0,1,3,3,3,5,5,8,8,19,19,21,21,21,22,22,22,22,22,22,22,22,22,22,22,22,23,23,23,23,23,24,24,24,24,24,26,26,29,29,29,29,29,3,3,3,3};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, UrlsPage page) {
 		jteOutput.writeContent("\n");
 		gg.jte.generated.ondemand.layout.JtepageGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
 			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
 				jteOutput.writeContent("\n    <table class=\"table table-striped\">\n        <thead>\n        <tr>\n            <th scope=\"col\">#</th>\n            <th scope=\"col\">URL</th>\n            <th scope=\"col\">Последний результат</th>\n            <th scope=\"col\">Код ответа</th>\n        </tr>\n        </thead>\n        <tbody>\n        ");
-				for (var p : page.getUrls()) {
+				for (var p : page.getUrlTuples()) {
 					jteOutput.writeContent("\n            <tr>\n                <td>");
 					jteOutput.setContext("td", null);
-					jteOutput.writeUserContent(p.getId());
+					jteOutput.writeUserContent(p.getValue0().getId());
 					jteOutput.writeContent("</td>\n                <td><a");
-					var __jte_html_attribute_0 = NamedRoutes.urlPath(p.getId());
+					var __jte_html_attribute_0 = NamedRoutes.urlPath(p.getValue0().getId());
 					if (gg.jte.runtime.TemplateUtils.isAttributeRendered(__jte_html_attribute_0)) {
 						jteOutput.writeContent(" href=\"");
 						jteOutput.setContext("a", "href");
@@ -24,8 +24,18 @@ public final class JteindexGenerated {
 					}
 					jteOutput.writeContent("> ");
 					jteOutput.setContext("a", null);
-					jteOutput.writeUserContent(p.getName());
-					jteOutput.writeContent("</a></td>\n                <td></td>\n                <td></td>\n            </tr>\n        ");
+					jteOutput.writeUserContent(p.getValue0().getName());
+					jteOutput.writeContent("</a></td>\n                <td>");
+					if (p.getValue1() != null) {
+						jteOutput.setContext("td", null);
+						jteOutput.writeUserContent(p.getValue1().getCreatedAt().toString());
+					}
+					jteOutput.writeContent("</td>\n                <td>");
+					if (p.getValue1() != null) {
+						jteOutput.setContext("td", null);
+						jteOutput.writeUserContent(p.getValue1().getStatusCode());
+					}
+					jteOutput.writeContent("</td>\n            </tr>\n        ");
 				}
 				jteOutput.writeContent("\n        </tbody>\n    </table>\n");
 			}
--- app/src/main/resources/init.sql
+++ app/src/main/resources/init.sql
@@ -5,3 +5,15 @@ create table urls(
     name varchar(150) unique not null,
     created_at timestamp not null
 );
+
+drop table if exists url_checks;
+
+create table url_checks(
+    id bigint generated always as identity primary key,
+    status_code int not null,
+    title varchar(150),
+    h1 varchar(150),
+    description text,
+    url_id bigint references urls(id),
+    created_at timestamp not null
+);
