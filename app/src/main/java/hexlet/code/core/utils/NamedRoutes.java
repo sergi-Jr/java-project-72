7daaff8f2244b9249266e066e61c884cf889f5bf
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-04-30 04:19:42 +0400

    [skip ci][skip render] add url check route

--- app/src/main/java/hexlet/code/core/utils/NamedRoutes.java
+++ app/src/main/java/hexlet/code/core/utils/NamedRoutes.java
@@ -16,4 +16,12 @@ public final class NamedRoutes {
     public static String urlPath(String id) {
         return "/urls/" + id;
     }
+
+    public static String urlCheckPath(Long id) {
+        return urlCheckPath(String.valueOf(id));
+    }
+
+    public static String urlCheckPath(String id) {
+        return "/urls/" + id + "/checks";
+    }
 }
