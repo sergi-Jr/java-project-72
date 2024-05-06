422a09d2cc88357dfbed247fdf5a52f05ecbe753
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-05-06 14:36:51 +0400

    [skip render] rework datasource sql initializtion

--- app/src/main/java/hexlet/code/core/dal/DataSource.java
+++ app/src/main/java/hexlet/code/core/dal/DataSource.java
@@ -4,7 +4,12 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.Getter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public final class DataSource {
@@ -14,12 +19,30 @@ public final class DataSource {
     static {
         HikariConfig config = new HikariConfig();
         String jdbcUrl = System.getenv().getOrDefault("JDBC_DATABASE_URL",
-                "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;INIT=runscript from 'classpath:/init.sql'");
+                "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
         config.setJdbcUrl(jdbcUrl);
         config.setUsername(System.getenv("USERNAME"));
         config.setPassword(System.getenv("PASSWORD"));
 
         dataSource = new HikariDataSource(config);
+
+        try (Connection conn = getConnection();
+             InputStream stream = DataSource.class.getResourceAsStream("/init.sql");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line).append("\n");
+            }
+
+            String query = builder.toString();
+            PreparedStatement prep = conn.prepareStatement(query);
+            prep.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
